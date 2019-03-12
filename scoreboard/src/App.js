5a29d0ac509802abcf9fc7e9e12308629979d9c0
Emily McClanahan <efmcclanahan@gmail.com>
2019-03-12 18:20:22 -0500

    adds the folder tree structure and a single render test

--- scoreboard/src/App.js
+++ scoreboard/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Display from './Components/Display/display';
+import Dashboard from './Components/Dashboard/dashboard';
 import './App.css';
 
 class App extends Component {
@@ -6,7 +8,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-         <h1>Scoreboard</h1>
+          <h1>Scoreboard</h1>
+          <Display />
+          <Dashboard />
         </header>
       </div>
     );
